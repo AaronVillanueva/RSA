757f0c12247d3693473b931f416353f5dfc6e6fc
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2019-06-28 09:46:50 -0500

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,3 +1,4 @@
+import numbers
 def llave(p1,p2):
   #dos numeros primos
   p1=p1
@@ -5,11 +6,16 @@ def llave(p1,p2):
   
   #multiplicar
   n=p1*p2
-  print(mcd(p1-1,p2-1))
+
+  #obtener mcd
+  minc=mcd(p1-1,p2-1)
+  #obtener el coprimo de minc. segundo valor el min. se obtiene el cercano
+  cop(minc,1)
+  print(1%780)
+  print(413*17)
 
 
 def mcd(num1,num2):
-
   if(num1>num2):
     alto=num1
   else:
@@ -21,5 +27,14 @@ def mcd(num1,num2):
     else:
       alto+=1
   return alto
+
+def cop(num,ini):
+  i=ini
+  while (True):
+    check=num/i
+    if(int(check)!=check):
+      break
+    i+=1
+  return check
 
 llave(61,53)
