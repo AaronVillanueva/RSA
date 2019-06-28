cdabe8b6ab75c82bed053f39adc3d2b2c3120a8f
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2019-06-28 09:17:29 -0500

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,1 +1,25 @@
-n1=2
+def llave(p1,p2):
+  #dos numeros primos
+  p1=p1
+  p2=p2
+  
+  #multiplicar
+  n=p1*p2
+  print(mcd(p1-1,p2-1))
+
+
+def mcd(num1,num2):
+
+  if(num1>num2):
+    alto=num1
+  else:
+    alto=num2
+  
+  while(True):
+    if((alto%num1==0) and (alto%num2==0)):
+      break
+    else:
+      alto+=1
+  return alto
+
+llave(61,53)
